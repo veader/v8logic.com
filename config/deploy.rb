005209ea99731cc66295a6e0fae3c7592d911202
Shawn Veader <shawn@veader.org>
2009-04-20 12:47:16 -0400

    adding capistrano tasks for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :application,   "v8logic"
+set :scm,           'git'
+set :repository,    "git@veader.org:#{application}"
+set :deploy_to,     "/var/www/#{application}"
+set :user,          'web'
+set :keep_releases, 5
+set :use_sudo,      false
+set :deploy_via,    :copy
+set :port,          2222
+
+role :web,          "v8logic.com"
+# role :app,        "v8logic.com"
+# role :db,         "v8logic.com, :primary => true
+
+deploy.task :restart, :roles => :web do
+  # DO NOTHING
+end
+
+deploy.task :finalize_update, :except => { :no_release => true } do
+  # DO NOTHING
+end
