f47d15c99006b5835231d2df2586cede16a26b44
Shawn Veader <shawn@veader.org>
2012-10-09 21:56:42 -0400

    updating deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,23 @@
-set :application,   "v8logic"
+set :application,   'v8logic'
 set :scm,           'git'
 set :repository,    "git@veader.org:#{application}"
 set :deploy_to,     "/var/www/#{application}"
 set :user,          'web'
-set :keep_releases, 5
+set :keep_releases, 3
 set :use_sudo,      false
 set :deploy_via,    :copy
-set :port,          2222
+set :port,          22
 
-role :web,          "v8logic.com"
-# role :app,        "v8logic.com"
-# role :db,         "v8logic.com, :primary => true
+role :web,          'v8logic.com'
 
-after 'deploy:symlink', :symlink_betas
+# after 'deploy:symlink', :symlink_betas
 
-task :symlink_betas, :roles => :web do
-  run <<-CMD
-    rm -f #{latest_release}/betas &&
-    ln -nfs #{shared_path}/betas #{latest_release}/betas
-  CMD
-end
+# task :symlink_betas, :roles => :web do
+#   run <<-CMD
+#     rm -f #{latest_release}/betas &&
+#     ln -nfs #{shared_path}/betas #{latest_release}/betas
+#   CMD
+# end
 
 deploy.task :restart, :roles => :web do
   # DO NOTHING
